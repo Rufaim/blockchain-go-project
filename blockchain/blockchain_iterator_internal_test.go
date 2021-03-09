34c0cde672cbfaa5df368c9d541be2a288bfdfe6
Pavel Rumiantsev <pavelrumyancew@yandex.ru>
2021-03-09 18:50:55 -0500

    tests for blockchain iterator

--- blockchain/blockchain_iterator_internal_test.go
+++ blockchain/blockchain_iterator_internal_test.go
@@ -1,11 +1,15 @@
+//+build tests
+
 package blockchain
 
 import (
 	"bytes"
 	"encoding/hex"
+	"fmt"
 	"testing"
 
 	pb "github.com/Rufaim/blockchain/message"
+	"github.com/Rufaim/blockchain/wallet"
 )
 
 type mockBlockchainIterator struct {
@@ -72,14 +76,108 @@ func TestBlockchainIterator_FindTransactionsByID(t *testing.T) {
 			}
 			if len(got) != len(tt.want) {
 				t.Errorf("BlockchainIterator.FindTransactionsByID() returned wrong number of results")
+				return
 			}
 			for key, val := range tt.want {
 				tx, ok := got[key]
 				if !ok {
 					t.Errorf("BlockchainIterator.FindTransactionsByID() not returned transaction of id %s", string(val.Id))
+					return
 				}
 				if bytes.Compare(tx.Id, val.Id) != 0 {
 					t.Errorf("BlockchainIterator.FindTransactionsByID() returned transaction for id %s", string(val.Id))
+					return
+				}
+			}
+		})
+	}
+}
+
+func TestBlockchainIterator_FindUnspentTransactions(t *testing.T) {
+	pubKey1 := []byte("aaa")
+	pubKey1Hash := wallet.HashPubKey(pubKey1)
+	pubKey2 := []byte("bbb")
+	pubKey2Hash := wallet.HashPubKey(pubKey2)
+	blockList := []*block{
+		{pb.Block{
+			PrevHash: []byte("0"), // we know taht genesis does not have previous hash at all
+			Transactions: []*pb.Transaction{
+				{Id: []byte("123"),
+					Inps: []*pb.TXInput{{Id: []byte("345"), OutId: 0, PubKey: pubKey1}},
+					Outs: []*pb.TXOutput{{Amount: 2, PubKeyHash: []byte("jnea")}, {Amount: 8, PubKeyHash: pubKey1Hash}},
+				},
+				// {Id: []byte("234")},
+			},
+		}},
+		{pb.Block{
+			PrevHash: []byte("0"),
+			Transactions: []*pb.Transaction{
+				{Id: []byte("456"),
+					Inps: []*pb.TXInput{{Id: []byte("456"), OutId: 0, PubKey: []byte("jnea")}},
+					Outs: []*pb.TXOutput{{Amount: 2, PubKeyHash: pubKey2Hash}},
+				},
+				{Id: []byte("345"), // coinbase key 1
+					Inps: []*pb.TXInput{{OutId: -1}},
+					Outs: []*pb.TXOutput{{Amount: 10, PubKeyHash: pubKey1Hash}},
+				},
+			},
+		}},
+		{pb.Block{
+			PrevHash: []byte("0"),
+			Transactions: []*pb.Transaction{
+				{Id: []byte("456"),
+					Inps: []*pb.TXInput{{Id: []byte("678"), OutId: 2, PubKey: []byte("afdbmad")}},
+					Outs: []*pb.TXOutput{{Amount: 3, PubKeyHash: []byte("jnea")}},
+				},
+				{Id: []byte("345"), // coinbase key 1
+					Inps: []*pb.TXInput{{OutId: -1}},
+					Outs: []*pb.TXOutput{{Amount: 10, PubKeyHash: pubKey1Hash}},
+				},
+				{Id: []byte("567"), // coinbase key 2
+					Inps: []*pb.TXInput{{OutId: -1}},
+					Outs: []*pb.TXOutput{{Amount: 10, PubKeyHash: pubKey2Hash}},
+				},
+			},
+		}},
+		{},
+	}
+
+	tests := []struct {
+		name string
+		key  []byte
+		want [][]byte
+	}{
+		{"key 1", pubKey1Hash, [][]byte{[]byte("123"), []byte("345")}},
+		{"key 2", pubKey2Hash, [][]byte{[]byte("456"), []byte("567")}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			bi := &mockBlockchainIterator{
+				blockList: blockList,
+				curretPos: -1,
+			}
+			got, err := FindUnspentTransactions(bi, tt.key)
+
+			for _, v := range got {
+				fmt.Println(string(v.Id))
+			}
+			fmt.Println()
+			for _, v := range tt.want {
+				fmt.Println(string(v))
+			}
+
+			if err != nil {
+				t.Errorf("FindUnspentTransactions() error = %v", err)
+				return
+			}
+			if len(got) != len(tt.want) {
+				t.Errorf("FindUnspentTransactions() returned wrong number of results")
+				return
+			}
+			for i := range tt.want {
+				if bytes.Compare(tt.want[i], got[i].Id) != 0 {
+					t.Errorf("FindUnspentTransactions() returned transaction for id %s", string(got[i].Id))
+					return
 				}
 			}
 		})
